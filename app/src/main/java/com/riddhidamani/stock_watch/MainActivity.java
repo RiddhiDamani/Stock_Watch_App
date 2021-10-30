3181fbb35fb6f3633ae02766ecd9ca26d93a2cc6
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-29 23:58:36 -0500

    Added Dialog Box

--- app/src/main/java/com/riddhidamani/stock_watch/MainActivity.java
+++ app/src/main/java/com/riddhidamani/stock_watch/MainActivity.java
@@ -1,13 +1,20 @@
 package com.riddhidamani.stock_watch;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener  {
 
@@ -15,6 +22,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.addStockMenu) {
+            addStockToMainDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addStockToMainDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        EditText editText = new EditText(this);
+        editText.setInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        editText.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        builder.setView(editText);
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        builder.setMessage("Please enter a Stock Symbol:");
+        builder.setTitle("Stock Selection");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
